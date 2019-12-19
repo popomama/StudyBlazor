58c052cd43f48003cf33176fb9d807191194b9db
Wei Xia <wei_xia@hotmail.com>
2019-12-19 15:14:52 -0800

    Add project files.

---
+++ Study/Pages/Learn Blazor/LearnRazor.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Study.Pages.Learn_Blazor
+{
+    public class LearnRazorBase: ComponentBase
+    {
+
+        protected string name = "spark";
+
+        protected string txtWelcome = "Time to learn Blazor!";
+
+        protected void getname()
+        {
+            name = "Blazoer Learner";
+        }
+    }
+}
---
+++ Study/Services/CategoryService.cs
@@ -0,0 +1,65 @@
+﻿using Study.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Study.Services
+{
+    public class CategoryService 
+    {
+
+        readonly ApplicationDbContext _db;
+
+        public CategoryService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Category GetCategory(int Id)
+        {
+            //Category cat = new Category();
+            return _db.Categories.FirstOrDefault(u => u.Id == Id);
+        }
+
+        public List<Category> GetCategories()
+        {
+            //Category cat = new Category();
+            return _db.Categories.ToList();
+        }
+
+        public bool CreateCategory(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            var ExistingCategory = _db.Categories.FirstOrDefault(u => u.Id == category.Id);
+            if (ExistingCategory != null)
+            {
+                ExistingCategory.Name = category.Name;
+                _db.SaveChanges();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            var ExistingCategory = _db.Categories.FirstOrDefault(u => u.Id == category.Id);
+            if (ExistingCategory != null)
+            {
+                _db.Categories.Remove(ExistingCategory);
+                _db.SaveChanges();
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
